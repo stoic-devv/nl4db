2691507c5ff82e61ad7da2f61247aec540108264
Aditya <adityakanthale@gmail.com>
2023-04-03 19:18:18 -0500

    insert imdb online sql dump

---
+++ scripts/imdb_preprocessing.sql
@@ -0,0 +1,4 @@
+
+create database imdbold template='template0' encoding='UTF8' lc_ctype='und-x-icu' lc_collate='und-x-icu';
+create database imdbold template='template0' encoding='UTF8' lc_ctype='C' lc_collate='C';
+
